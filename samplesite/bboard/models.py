7f2fee1f16e8173fe755ece9d18d07811ad762b2
Evgenii Myshevskii <e.g.myshevskiy@gmail.com>
2022-04-28 16:55:25 +0300

    Add model and django amdin panel

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Deal title")
+    content = models.TextField(null=True, blank=True, verbose_name="Deal description")
+    price = models.FloatField(null=True, blank=True, verbose_name="Price")
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Published")
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = "Deal"
+        verbose_name_plural = "Deals"
+        ordering = ["-published"]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from bboard.models import Bb
 
-# Create your views here.
+def index(request):
+    template_name = 'bboard/index.html'
+    deals = Bb.objects.all()
+    context = {'bbs': deals}
+    return render(request, template_name=template_name, context=context)
